313a884e78d0c48bd604791abff24c2b0a55aeab
Rudradev-Arya <rudradev93@gmail.com>
2025-03-27 17:01:30 +0530

    added: swagger documentation at /docs endpoint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const schoolRoutes = require('./routes/schools');
 
 const app = express();
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
