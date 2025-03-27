f6b2f584abea37ff7985023c71128bfb0259d598
Rudradev-Arya <rudradev93@gmail.com>
2025-03-27 16:54:20 +0530

    added: addschl and listschl routes

---
+++ src/controllers/schoolController.js
@@ -0,0 +1,27 @@
+const School = require('../models/School');
+
+exports.addSchool = async (req, res) => {
+  try {
+    const { name, address, latitude, longitude } = req.body;
+    if (!name || !address || !latitude || !longitude) {
+      return res.status(400).json({ error: 'All fields are required vroo' });
+    }
+    const id = await School.add(name, address, latitude, longitude);
+    res.status(201).json({ id, message: 'School added successfully bossman' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error adding school' });
+  }
+};
+
+exports.listSchools = async (req, res) => {
+  try {
+    const { latitude, longitude } = req.query;
+    if (!latitude || !longitude) {
+      return res.status(400).json({ error: 'Latitude and longitude are required' });
+    }
+    const schools = await School.list(latitude, longitude);
+    res.json(schools);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching schools' });
+  }
+};
